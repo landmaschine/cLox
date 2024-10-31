fb9872da3e758c484255b6d6aa9ef34fd472a7a6
landmaschine <leon@echoton.com>
2024-10-31 16:03:09 +0100

    update

--- src/scanner.c
+++ src/scanner.c
@@ -200,7 +200,7 @@ Token scanToken() {
                 match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
         case '>':
             return makeToken(
-                match('=') ? TOKEN_GREATE_EQUAL : TOKEN_GREATER);
+                match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
         case '"': return string();
     }
 
